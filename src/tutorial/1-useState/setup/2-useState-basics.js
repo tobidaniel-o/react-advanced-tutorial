84787b695fc55351b7054ff0f5995fa91fbc00ac
Tobi <tobi.daniel2018@gmail.com>
2021-03-21 13:19:48 -0400

    Initial commit

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,6 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateBasics = () => {
+  // console.log(useState("hello world"));
+  // const value = useState(1)[0];
+  // const handler = useState(1)[1];
+  // console.log(value, handler);
+
+  const [text, setText] = useState()
   return <h2>useState basic example</h2>;
 };
 
