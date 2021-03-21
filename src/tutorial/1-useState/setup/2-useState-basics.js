1fd21284680a0efd9514d079c410720305304d57
Tobi <tobi.daniel2018@gmail.com>
2021-03-21 14:12:07 -0400

    learning useState with array

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -6,8 +6,22 @@ const UseStateBasics = () => {
   // const handler = useState(1)[1];
   // console.log(value, handler);
 
-  const [text, setText] = useState()
-  return <h2>useState basic example</h2>;
+  const [text, setText] = useState("random title");
+  const handleClick = () => {
+    if (text === "random title") {
+      setText("hello world");
+    } else {
+      setText("random title");
+    }
+  };
+  return (
+    <React.Fragment>
+      <h1>{text}</h1>
+      <button className="btn" onClick={handleClick}>
+        Change title
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default UseStateBasics;
--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,33 @@
-import React from 'react';
-import { data } from '../../../data';
+import React from "react";
+import { data } from "../../../data";
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = React.useState(data);
+
+  // How to remove an item from a list of items
+  const removeItem = (id) => {
+    let newPeople = people.filter((person) => person.id !== id);
+
+    // Updates the 'people' state
+    setPeople(newPeople);
+  };
+
+  return (
+    <>
+      {people.map((person) => {
+        const { id, name } = person;
+        return (
+          <div key={id} className="item">
+            <h4>{name}</h4>
+            <button onClick={() => removeItem(id)}>remove</button>
+          </div>
+        );
+      })}
+      <button className="btn" onClick={() => setPeople([])}>
+        Clear items
+      </button>
+    </>
+  );
 };
 
 export default UseStateArray;
